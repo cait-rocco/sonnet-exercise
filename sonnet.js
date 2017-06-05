fbbe94576b3b7d01804fe393f637454faa5958bd
Cait Rocco <cait.rocco@gmail.com>
2017-06-05 15:14:24 -0500

    put tasks in js file

---
+++ sonnet.js
@@ -0,0 +1,21 @@
+// Take the contents of the sonnet div and place it in a variable.
+
+var sonnetText = ""
+
+// Find and output the starting position of the word "orphans".
+
+
+
+// Output the number of characters in the sonnet.
+
+
+
+// Replace the first occurance of the string "winter" with "yuletide".
+
+
+
+// Replace all occurances of the string "the" with "a large".
+
+
+
+// Set the content of the sonnet div with the new string.
